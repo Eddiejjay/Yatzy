127a8d418dc272c5c34d32f342f3f2ef16c6bdbc
Eero Mäki-Kokkila <eerojmk@gmail.com>
2021-12-12 22:02:38 +0200

    update

--- cypress/integration/yatzy_app.js
+++ cypress/integration/yatzy_app.js
@@ -4,4 +4,11 @@ describe('Yatzy', function() {
       cy.contains('Login')
       cy.contains('Wanna roll some dices?')
     })
-  })
+
+    it('login form can be opened', function() {
+        cy.visit('http://localhost:3003')
+        cy.contains('Login').click()
+        cy.contains('Log `n` roll! ')
+
+      })
+    })
